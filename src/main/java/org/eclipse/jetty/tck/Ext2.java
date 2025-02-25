fdfabbf85356be3d211e5c249c11a45d175f2134
Lachlan Roberts <lachlan.p.roberts@gmail.com>
2025-02-25 16:07:38 +1100

    Add extension implementations required for WebSocket TCK
    Signed-off-by: Lachlan Roberts <lachlan.p.roberts@gmail.com>

---
+++ src/main/java/org/eclipse/jetty/tck/Ext2.java
@@ -0,0 +1,10 @@
+package org.eclipse.jetty.tck;
+
+public class Ext2 extends org.eclipse.jetty.websocket.core.AbstractExtension
+{
+    @Override
+    public String getName()
+    {
+        return "ext2";
+    }
+}
